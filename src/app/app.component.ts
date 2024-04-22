d782269061f761bb524340c664157c1b1aaeb7a2
ASathvikReddy <21mh1a05e0@acoe.edu.in>
2024-04-22 21:52:16 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { flatMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angularapp';
+  name: string = "Mean Stack";
+  cse = {
+    fname: "Durga",
+    lname: "Prasad",
+  };
+  csecolor: string = "red";
+  csestyle: object = {
+    color: "white",
+    background: "red",
+    border: '5px solid black',
+  };
+  rating: number = 5;
+  check: boolean = true;
+  check1: boolean = false;
+  cses: boolean = true;
+  cses1: boolean = false;
+  c1apply: boolean = true;
+  c2apply: boolean = true;
+  path: string = "./assets/Screenshot (290).png";
+  styles: string = "box";
+  names: string = "Durga Prasad";
+  rocks() {
+    this.names = "MNST Faculty";
+  }
+  disp() {
+    console.log("Clicked ON Button");
+  }
 }
